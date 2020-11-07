bdacd658879c907081ade8848f7d23277dbacd5c
VongolaTeong <0522teongkarchun@gmail.com>
2020-11-07 16:24:16 +0800

    Update train.py
    correctly read onehots

--- train.py
+++ train.py
@@ -6,7 +6,8 @@ import numpy as np
 names = np.genfromtxt("train/names_labels.txt", dtype=str, delimiter = ',', skip_header = 1, usecols = (0))
 #read labels from file
 labels = np.genfromtxt('train/names_labels.txt', delimiter = ',', skip_header = 1, usecols = (1))
-#read one-hots from file
-data = np.load('train/names_onehots.npy', allow_pickle = True)
-data.item()
+
+#read onehots from file
+data = np.load('train/names_onehots.npy', allow_pickle = True).item()
 onehots = data['onehots']
+
