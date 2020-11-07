7bbf21c60d88a8b1432faeba57c441fd2cd5a91d
VongolaTeong <0522teongkarchun@gmail.com>
2020-11-07 13:46:44 +0800

    Update train.py
    read data from files

--- train.py
+++ train.py
@@ -1,1 +1,8 @@
 #source file for training
+
+import numpy as np
+
+#read names from file
+names = np.genfromtxt("train/names_labels.txt", dtype=str, delimiter = ',', skip_header = 1, usecols = (0))
+#read labels from file
+labels = np.genfromtxt('train/names_labels.txt', delimiter = ',', skip_header = 1, usecols = (1))
