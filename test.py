af0b4b69fd9158f68c678d68d99f8261d5b21ce5
VongolaTeong <0522teongkarchun@gmail.com>
2020-11-08 14:25:54 +0800

    Update test.py
    preprocess data

--- test.py
+++ test.py
@@ -6,3 +6,6 @@ import tensorflow as tf
 #read onehots from file
 data = np.load('test/names_onehots.npy', allow_pickle = True).item()
 onehots = data['onehots']
+
+#reshape onehots for input purpose
+onehots = np.reshape(onehots, [len(onehots), 1, 73, 398])
