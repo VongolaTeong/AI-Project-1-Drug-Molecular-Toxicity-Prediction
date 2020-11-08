e32aafbea15da521707d5cb58bf1e6def55d1d44
VongolaTeong <0522teongkarchun@gmail.com>
2020-11-08 13:34:23 +0800

    Update test.py
    read test data

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
 #source file for recovering the network model
+
+import numpy as np
+import tensorflow as tf
+
+#read onehots from file
+data = np.load('test/names_onehots.npy', allow_pickle = True).item()
+onehots = data['onehots']
