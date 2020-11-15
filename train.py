4e5caa61126d77aa5012f1cdab517f744e979f38
VongolaTeong <0522teongkarchun@gmail.com>
2020-11-15 15:52:09 +0800

    Update train.py
    add comments and indents

--- train.py
+++ train.py
@@ -5,9 +5,11 @@ import tensorflow as tf
 import pandas as pd
 import os
 
+#set a constant seed value to get consistent results
 seed_value = 100
 tf.set_random_seed(seed_value)
 
+#function to load datasets
 def load_data(filefolder):
     data = np.load(os.path.abspath(filefolder + '/names_onehots.npy'), allow_pickle=True).item()
     data = data['onehots']
@@ -15,16 +17,19 @@ def load_data(filefolder):
     label = label['Label'].values
     return data, label
 
+#model class
 class MyModel(tf.keras.Model):
     def __init__(self):
         super(MyModel, self).__init__()
+        #set layers
         self.conv1_layer = tf.keras.layers.Conv2D(32, 5, 1, 'same', activation=tf.nn.relu)
         self.pool1_layer = tf.keras.layers.MaxPool2D(2, 2)
         self.conv2_layer = tf.keras.layers.Conv2D(32, 3, (1, 2), 'same', activation=tf.nn.relu)
         self.pool2_layer = tf.keras.layers.MaxPool2D(2, 2)
-        # flat
+        
+        #flat
         self.FCN = tf.keras.layers.Dense(2)
-        # softmax
+        #softmax
 
     def call(self, inputs):
         x = self.conv1_layer(inputs)
@@ -37,41 +42,44 @@ class MyModel(tf.keras.Model):
         return output, output_with_sm
 
 def run():
-    # parameters
+    #parameters
     LR = 0.01
     BatchSize = 128
     EPOCH = 2
 
     train_data_path = os.path.join(os.path.dirname(__file__), "train/")
     validation_data_path = os.path.join(os.path.dirname(__file__), "validation/")
-    # data
+
+    #data
     train_x, train_y = load_data(train_data_path)
     valid_x, valid_y = load_data(validation_data_path)
 
-
-    # model & input and output of model
+    #create model instance
     model = MyModel()
 
+    #input and output
     onehots_shape = list(train_x.shape[1:])
     input_place_holder = tf.placeholder(tf.float32, [None] + onehots_shape, name='input')
     input_place_holder_reshaped = tf.reshape(input_place_holder, [-1] + onehots_shape + [1])
     label_place_holder = tf.placeholder(tf.int32, [None], name='label')
     label_place_holder_2d = tf.one_hot(label_place_holder, 2)
     output, output_with_sm = model(input_place_holder_reshaped)
-    model.summary()  # show model's structure
 
-    # loss
-    bce = tf.keras.losses.BinaryCrossentropy()  # compute cost
+    #show model's structure
+    model.summary() 
+
+    #loss
+    bce = tf.keras.losses.BinaryCrossentropy()
     loss = bce(label_place_holder_2d, output_with_sm)
 
-    # Optimizer
+    #optimizer
     train_op = tf.train.AdamOptimizer(LR).minimize(loss)
 
-    # auc
+    #auc
     prediction_place_holder = tf.placeholder(tf.float64, [None], name='pred')
     auc, update_op = tf.metrics.auc(labels=label_place_holder, predictions=prediction_place_holder)
 
-    # run
+    #run
     init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
     with tf.Session() as sess:
         sess.run(init_op)
@@ -80,6 +88,7 @@ def run():
 
         train_size = train_x.shape[0]
         best_val_auc = 0
+        
         for epoch in range(EPOCH):
             for i in range(0, train_size, BatchSize):
                 b_x, b_y = train_x[i:i + BatchSize], train_y[i:i + BatchSize]
