3fa3744e61b810ad2d2f9d2aecd7c025aa8515e6
VongolaTeong <0522teongkarchun@gmail.com>
2020-11-15 15:52:06 +0800

    Update test.py
    add comments and indents

--- test.py
+++ test.py
@@ -5,22 +5,27 @@ import tensorflow as tf
 import os
 import statistics
 
+#set a constant seed value to get consistent results
 seed_value = 100
 tf.set_random_seed(seed_value)
 
+#function to load datasets
 def load_test_data_name(filefolder):
     data = np.load(os.path.abspath(filefolder + '/names_onehots.npy'), allow_pickle=True).item()
     onehots = data['onehots']
     name = data['names']
     return onehots, name
 
+#model class
 class MyModel(tf.keras.Model):
     def __init__(self):
         super(MyModel, self).__init__()
+        #set layers
         self.conv1_layer = tf.keras.layers.Conv2D(32, 5, 1, 'same', activation=tf.nn.relu)
         self.pool1_layer = tf.keras.layers.MaxPool2D(2, 2)
         self.conv2_layer = tf.keras.layers.Conv2D(32, 3, (1, 2), 'same', activation=tf.nn.relu)
         self.pool2_layer = tf.keras.layers.MaxPool2D(2, 2)
+        
         # flat
         self.FCN = tf.keras.layers.Dense(2)
         # softmax
@@ -35,19 +40,19 @@ class MyModel(tf.keras.Model):
         output_with_sm = tf.nn.softmax(output)
         return output, output_with_sm
     
-# data
+#data
 test_path = os.path.join(os.path.dirname(__file__), "test/")
 test_data, test_name = load_test_data_name(test_path)
 name = test_name
 
-# model
-tf.reset_default_graph()  # 
+#model
+tf.reset_default_graph() 
 model = MyModel()
 input_place_holder = tf.placeholder(tf.float32, [None] + list(test_data.shape[1:]), name='input')
 input_place_holder_reshaped = tf.reshape(input_place_holder, [-1] + list(test_data.shape[1:]) + [1])
 output, output_with_sm = model(input_place_holder_reshaped)
 
-# Predict on the test set
+#predict on the test set
 data_size = test_data.shape[0]
 BatchSize = 128
 with tf.Session() as sess:
@@ -55,24 +60,26 @@ with tf.Session() as sess:
     saver.restore(sess, 'weights/weights')
     prediction = []
     for i in range(0, data_size, BatchSize):
-        print(i)
+        #print(i)
         test_output = sess.run(output, {input_place_holder: test_data[i:i + BatchSize]})
         test_output_with_sm = sess.run(output_with_sm, {input_place_holder: test_data[i:i + BatchSize]})
         pred = test_output_with_sm[:, 1]
         prediction.extend(list(pred))
+        
 sess.close()
+
 f = open('output_518030990014.txt', 'w')
 f.write('Chemical,Label\n')
 
+#get the mean of prediction and use it as binary boundary
 mean = statistics.mean(prediction)
-for x, i in enumerate(prediction):
-    if prediction[x] > mean:
-        prediction[x] = 1
+for i, x in enumerate(prediction):
+    if x > mean:
+        prediction[i] = 1
     else:
-        prediction[x] = 0
+        prediction[i] = 0
 
-i = 0
-for x in prediction:
+for i, x in enumerate(prediction):
     f.write(name[i] + ',' + str(x) + '\n')
-    i += 1
+    
 f.close()
