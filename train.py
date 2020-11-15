16c1565d7ecea5708147fb93b8573a9e21c55caf
VongolaTeong <0522teongkarchun@gmail.com>
2020-11-15 15:41:21 +0800

    Update train.py
    use demo code

--- train.py
+++ train.py
@@ -2,76 +2,98 @@
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import layers, models
+import pandas as pd
 import os
 
-def createModel():
-    #set input shape according to the size
-    inputShape = (1, 73, 398)
-    
-    #create model
-    model = models.Sequential()
+seed_value = 100
+tf.set_random_seed(seed_value)
 
-    #add layers to the model
-    model.add(layers.Conv2D(16, (3, 3), activation='relu', input_shape = inputShape, padding = 'same'))
-    model.add(layers.MaxPooling2D((2, 2), padding = 'same'))
+def load_data(filefolder):
+    data = np.load(os.path.abspath(filefolder + '/names_onehots.npy'), allow_pickle=True).item()
+    data = data['onehots']
+    label = pd.read_csv(os.path.abspath(filefolder + '/names_labels.txt'), sep=',')
+    label = label['Label'].values
+    return data, label
 
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', padding = 'same'))
-    model.add(layers.MaxPooling2D((2, 2), padding = 'same'))
+class MyModel(tf.keras.Model):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.conv1_layer = tf.keras.layers.Conv2D(32, 5, 1, 'same', activation=tf.nn.relu)
+        self.pool1_layer = tf.keras.layers.MaxPool2D(2, 2)
+        self.conv2_layer = tf.keras.layers.Conv2D(32, 3, (1, 2), 'same', activation=tf.nn.relu)
+        self.pool2_layer = tf.keras.layers.MaxPool2D(2, 2)
+        # flat
+        self.FCN = tf.keras.layers.Dense(2)
+        # softmax
 
-    model.add(layers.Conv2D(64, (3, 3), activation='relu', padding = 'same'))
-
-    #flatten 3D output to 1D for dense layers
-    model.add(layers.Flatten())
-    #add dense layers to perform classification
-    model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(10))
-
-    #compile the model
-    model.compile(optimizer='adam',
-              loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-
-    return model
+    def call(self, inputs):
+        x = self.conv1_layer(inputs)
+        x = self.pool1_layer(x)
+        x = self.conv2_layer(x)
+        x = self.pool2_layer(x)
+        flat = tf.reshape(x, [-1, 18*50*32])
+        output = self.FCN(flat)
+        output_with_sm = tf.nn.softmax(output)
+        return output, output_with_sm
 
 def run():
-    #read names from file
-    names = np.genfromtxt("train/names_labels.txt", dtype=str, delimiter = ',', skip_header = 1, usecols = (0))
-    #read labels from file
-    labels = np.genfromtxt('train/names_labels.txt', delimiter = ',', skip_header = 1, usecols = (1))
+    # parameters
+    LR = 0.01
+    BatchSize = 128
+    EPOCH = 2
 
-    #read onehots from file
-    data = np.load('train/names_onehots.npy', allow_pickle = True).item()
-    onehots = data['onehots']
+    train_data_path = os.path.join(os.path.dirname(__file__), "train/")
+    validation_data_path = os.path.join(os.path.dirname(__file__), "validation/")
+    # data
+    train_x, train_y = load_data(train_data_path)
+    valid_x, valid_y = load_data(validation_data_path)
 
-    #read validation labels from file
-    validLabels = np.genfromtxt('validation/names_labels.txt', delimiter = ',', skip_header = 1, usecols = (1))
 
-    #read validation onehots from file
-    validData = np.load('validation/names_onehots.npy', allow_pickle = True).item()
-    validOnehots = validData['onehots']
+    # model & input and output of model
+    model = MyModel()
 
-    #get size of 2D matrix of onehots
-    size = onehots[0].shape
+    onehots_shape = list(train_x.shape[1:])
+    input_place_holder = tf.placeholder(tf.float32, [None] + onehots_shape, name='input')
+    input_place_holder_reshaped = tf.reshape(input_place_holder, [-1] + onehots_shape + [1])
+    label_place_holder = tf.placeholder(tf.int32, [None], name='label')
+    label_place_holder_2d = tf.one_hot(label_place_holder, 2)
+    output, output_with_sm = model(input_place_holder_reshaped)
+    model.summary()  # show model's structure
 
-    #reshape onehots for input purpose
-    onehots = np.reshape(onehots, [len(onehots), 1, 73, 398])
-    validOnehots = np.reshape(validOnehots , [len(validOnehots), 1, 73, 398])
+    # loss
+    bce = tf.keras.losses.BinaryCrossentropy()  # compute cost
+    loss = bce(label_place_holder_2d, output_with_sm)
 
-    model = createModel()
-    model.summary()
+    # Optimizer
+    train_op = tf.train.AdamOptimizer(LR).minimize(loss)
 
-    checkpoint_path = "weights/cp.ckpt"
-    checkpoint_dir = os.path.dirname(checkpoint_path)
+    # auc
+    prediction_place_holder = tf.placeholder(tf.float64, [None], name='pred')
+    auc, update_op = tf.metrics.auc(labels=label_place_holder, predictions=prediction_place_holder)
 
-    #create a callback that saves the model's weights
-    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath = checkpoint_path,
-                                                 save_weights_only = True,
-                                                 verbose = 1)
+    # run
+    init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+    with tf.Session() as sess:
+        sess.run(init_op)
 
-    #train the model with the new callback
-    model.fit(onehots, labels, epochs = 10, validation_data=(validOnehots, validLabels),
-          callbacks=[cp_callback])  #pass callback to training
+        saver = tf.train.Saver()
 
+        train_size = train_x.shape[0]
+        best_val_auc = 0
+        for epoch in range(EPOCH):
+            for i in range(0, train_size, BatchSize):
+                b_x, b_y = train_x[i:i + BatchSize], train_y[i:i + BatchSize]
+                _, loss_ = sess.run([train_op, loss], {'input:0': b_x, 'label:0': b_y})
+
+                print("Epoch {}: [{}/{}], training set loss: {:.4}".format(epoch, i, train_size, loss_))
+
+            if epoch % 1 == 0:
+                val_prediction = sess.run(output_with_sm, {'input:0': valid_x})
+                val_prediction = val_prediction[:, 1]
+                auc_value = sess.run(update_op, feed_dict={prediction_place_holder: val_prediction, label_place_holder: valid_y})
+                print("auc_value", auc_value)
+                if auc_value > best_val_auc:
+                    saver.save(sess, 'weights/weights')
+                    
 if __name__ == "__main__":
     run()
